556da2fd3c23b3f60023fda52c68e3daa6db706f
Keith Kovach <kovachks90@gmail.com>
2017-08-21 19:43:47 -0400

    twitter working

--- keys.js
+++ keys.js
@@ -2,7 +2,7 @@ console.log("this is loaded")
 
 exports.twitterKeys = {
 	consumer_key: 'gnc24JcjXPpypfdzt3eEFh0Qc',
-	consumer_secret: '	zpm2IIB9T8DV4ItJcyaUpRioOIXTzZ7iVANgWhDdZyvjOzAPBn',
-	access_token_key: '	899468192556478466-HDP1m2pqnje0pSgH4y3HY90NFYAyZXG',
+	consumer_secret: 'zpm2IIB9T8DV4ItJcyaUpRioOIXTzZ7iVANgWhDdZyvjOzAPBn',
+	access_token_key: '899468192556478466-HDP1m2pqnje0pSgH4y3HY90NFYAyZXG',
 	access_token_secret: 'VRN2OuIVSqJ1msp2tvTOQ40gNELe5ler1knioQCxTikUJ',
 }
--- liri.js
+++ liri.js
@@ -1,12 +1,32 @@
+var Twitter = require('twitter');
 
-var client = require('./keys.js');
+var TwitterKeys = require('./keys.js');
 
 var command = process.argv[2];
 
 var userRequest = process.argv[3];
 
+var client = new Twitter({
+	consumer_key: TwitterKeys.twitterKeys.consumer_key,
+	consumer_secret: TwitterKeys.twitterKeys.consumer_secret,
+	access_token_key: TwitterKeys.twitterKeys.access_token_key,
+	access_token_secret: TwitterKeys.twitterKeys.access_token_secret
+})
+
 if (command === 'my-tweets') {
-	console.log("twitter")
+	var package = {count: 20,
+				   result_type: 'recent'};
+	client.get('statuses/user_timeline', package, function(error, data, response) {
+		if (error) {
+			console.log(error)
+		} else {
+			var tweets = data;
+			for (var i = 0; i < 20; i += 1) {
+				console.log("\n" + data[i].text)
+			}
+		}
+	})
+
 } else if (command === 'spotify-this-song') {
 	console.log("spotify")
 } else if (command === 'movie-this') {
